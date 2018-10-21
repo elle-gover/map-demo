e7727bfec5c3b539758a312147cc80728cbeffe4
Elle Gover <elle.gover@detroitlabs.com>
2018-10-21 13:17:12 -0400

    initial commit with gitignore

---
+++ map-demo/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  map-demo
+//
+//  Created by Elle Gover on 10/21/18.
+//  Copyright © 2018 com.detroitlabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
