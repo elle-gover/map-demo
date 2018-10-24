106fcd7a573b34f6a4220835c3230b10f2a90806
Elle Gover <elle.gover@detroitlabs.com>
2018-10-24 10:12:31 -0400

    setting up MapViewController

---
+++ map-demo/Controllers/MapViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  map-demo
+//
+//  Created by Elle Gover on 10/21/18.
+//  Copyright © 2018 com.detroitlabs. All rights reserved.
+//
+
+import UIKit
+import Mapbox
+
+class MapViewController: UIViewController, MGLMapViewDelegate {
+
+    // MARK: - Properties
+    
+    var mapView = MGLMapView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    // MARK: - Methods
+    
+    /// TODO: Write createMap method to instantiate mapView
+    
+    
+}
+
